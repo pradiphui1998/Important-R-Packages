208c1fa26fcf90b1efb743b1f31478f7a40fef07
Pradip Hui <89596298+pradiphui1998@users.noreply.github.com>
2024-02-09 18:52:49 +0530

    Add files via upload

---
+++ Important packeges in R.R
@@ -0,0 +1,31 @@
+library(haven)          # read data set
+library (statpsych)     # Sample Size of WMV test
+# CI for a 2-group median difference)
+library(MKpower)        # Sample Size of WMV test
+library(samplesize)     # sample size of WMV test for ordinal scale
+library(ggplot2)        # EDF, histograms
+library(DescTools)      # check outliers
+library(rempsyc)        # check normality
+library(summarytools)   # summary statistics
+library(kableExtra)     # tables format
+library(ggstatsplot)    # box/violin plots with WMW & ES (rrb)
+library(sjstats)        # Mann-Whitney U Test & ES (r)
+library(stats)          # WMW test
+library(effectsize)     # ES (rrb, WMW odds, Cohen's d)
+library(asht)           # WMW test & ES (p)
+library(coin)           # Wilcoxon test with Hodges-Lehmann estimation,
+#van der Waerden test,
+#Fligner-Killeen & Ansari-Bradley tests
+library(rstatix)        # Wilcoxon test & ES (r)
+library(robustrank)     # combine WMW and Fligner-Policello test
+library(brunnermunzel)  # Brunner-Munzel test & ES (p)
+library(wmwpow)         # power of the WMW
+library(snpar)          # Quantile test
+library(quantreg)       # Quantile regression
+library (nptest)        # Studentized Wilcoxon test
+library (Rfit)          # Rank-based regression
+library(infer)          # bootstrap of CI for difference in the median
+library(car)            # Levene's test
+library(jtools)         # table for regression model
+library(tidyverse)      # data manipulation
+library(report)         # reporting of results
